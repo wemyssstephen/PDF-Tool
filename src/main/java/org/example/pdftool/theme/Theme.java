833f7c22b4316d60dd9eb52ee8b37a677a7b1bab
Stephen Wemyss <wemyssstephen@gmail>
2025-02-08 23:09:20 +0000

    Much better styling

---
+++ src/main/java/org/example/pdftool/theme/Theme.java
@@ -0,0 +1,9 @@
+package org.example.pdftool.theme;
+
+public class Theme {
+    public static final String PRIMARY = "#1a73e8";
+    public static final String BACKGROUND = "#202124";
+    public static final String SURFACE = "#292a2d";
+    public static final String TEXT_PRIMARY = "#e8eaed";
+    public static final String TEXT_SECONDARY = "#9aa0a6";
+}
